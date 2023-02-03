c0d9e961dea243daf2018508f16ae795d5943fa7
Henry Ginn <henryginn137@gmail.com>
2023-02-03 14:26:13 +0000

    Updated documentation

--- Side Band Fitting Interface.py
+++ Side Band Fitting Interface.py
@@ -2,16 +2,12 @@ from DataSet import DataSet
 
 """
 Before using the program, there are several things you need to check
-1: What is the folder structure? Details on this are found in DataSet
+1: What is the folder structure?
 2: Are the folder and file names what the program is expecting?
-   If there is a program that contains a data set with the same
-   structure as yours that is intended to fix names, you should
-   check that file for any notes,
-   The main cause of error are numberings such as 0, 1, ..., 10, 11.
-   When sorting as a string this will get 0, 10, 1, 11, so numbers
-   should be padded with leading 0s: 00, 01, ..., 10, 11.
 3: Is your repository saved in the correct place relative to the
-   data set? Details about this are given in Side Band Docs.
+   data set?
+
+Details about how to check all of these are given in the README.
 """
 
 #my_data_set = DataSet("15112022", folder_structure_type=1)
