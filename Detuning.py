667b42f86126c16fdfd47011cedd7e136e48603e
Henry Ginn <henryginn137@gmail.com>
2023-02-09 12:57:14 +0000

    Gamma is found

--- Detuning.py
+++ Detuning.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+import scipy as sc
 from Spectrum import Spectrum
 
 plt.rcParams['font.size'] = 12
@@ -14,22 +16,14 @@ class Detuning():
     """
 
     parameter_names = ["F", "Gamma", "Noise", "w"]
-
-    fitting_choice_functions = {"1": (self.update_fitting_parameter, 0),
-                                    "2": (self.update_fitting_parameter, 1),
-                                    "3": (self.update_fitting_parameter, 2),
-                                    "4": (self.update_fitting_parameter, 3),
-                                    "5": self.reset_fitting_parameters,
-                                    "6": self.update_all_fitting_parameters,
-                                    "7": self.reattempt_automatic_fit,
-                                    "8": self.accept_fit,
-                                    "": self.reject_fit}
+    bad_fit_threshold = 20
     
     def __init__(self, trial, detuning, timestamp, transmission_path, spectrum_paths):
         self.initialise_attributes(trial, detuning, timestamp,
                                    transmission_path, spectrum_paths)
         self.set_frequency()
         self.create_spectrum_objects()
+        self.set_fitting_choice_functions()
 
     def initialise_attributes(self, trial, detuning, timestamp,
                               transmission_path, spectrum_paths):
@@ -57,11 +51,22 @@ class Detuning():
     def create_spectrum_objects(self):
         self.spectrum_objects = [Spectrum(self, spectrum_path)
                                  for spectrum_path in self.spectrum_paths]
+
+    def set_fitting_choice_functions(self):
+        self.fitting_choice_functions = {"1": (self.update_fitting_parameter, 0),
+                                         "2": (self.update_fitting_parameter, 1),
+                                         "3": (self.update_fitting_parameter, 2),
+                                         "4": (self.update_fitting_parameter, 3),
+                                         "5": (self.reset_fitting_parameters, ),
+                                         "6": (self.update_all_fitting_parameters, ),
+                                         "7": (self.reattempt_automatic_fit, ),
+                                         "8": (self.accept_fit, ),
+                                         "": (self.reject_fit, )}
     
     def set_gamma(self):
         self.set_average_S21()
         self.initial_fitting_parameters = self.get_initial_fitting_parameters()
-        self.fitting_parameters = self.get_automatic_fit(initial_fitting_parameters)
+        self.fitting_parameters = self.get_automatic_fit(self.initial_fitting_parameters)
         self.gamma = self.get_gamma_from_fit()
     
     def set_average_S21(self):
@@ -99,7 +104,7 @@ class Detuning():
 
     def get_initial_fitting_parameters(self):
         end = int(len(self.S21)/5)
-        noise, w = mean(self.S21[:end])/20, 2
+        noise, w = np.mean(self.S21[:end])/20, 2
         K = np.mean(self.S21[self.S21 >= np.max(self.S21)*2/3])
         k = K * 1/3
         frequency_resolution = self.frequency[1] - self.frequency[0]
@@ -111,17 +116,17 @@ class Detuning():
         return initial_fitting_parameters
 
     def get_automatic_fit(self, initial_fitting_parameters):
-        fitting_parameters = leastsq(self.get_residuals,
-                                     initial_fitting_parameters)
+        fitting_parameters = sc.optimize.leastsq(self.get_residuals,
+                                                 initial_fitting_parameters)[0]
         return fitting_parameters
 
-    def get_residuals(self, p):
+    def get_residuals(self, fitting_parameters):
         residuals = self.evaluate_lorentzian(fitting_parameters) - self.S21
         return residuals
 
     def evaluate_lorentzian(self, lorentzian_parameters):
         F, gamma, noise, w = lorentzian_parameters
-        function_values = (F/(gamma**2 + 4*(self.frequency - w)**2)) + noise
+        function_values = (F/(gamma**2 + 4*(self.frequency_offset - w)**2)) + noise
         return function_values
 
     def get_gamma_from_fit(self):
@@ -133,12 +138,18 @@ class Detuning():
         return gamma
 
     def is_plot_badly_fitted(self):
-    fit_ratio = self.fitting_parameters[:2]/self.initial_fitting_parameters[:2]
-    fit_heuristic = sum(fit_ratio + 1/fit_ratio) + abs(self.fitting_parameters[3] - self.initial_fitting_parameters[3])
-    if fit_heuristic > 20:
-        print(f"Fit heuristic: {fit_heuristic}")
-        return True
-    return False
+        fit_heuristic = self.get_fit_heuristic()
+        if fit_heuristic > self.bad_fit_threshold:
+            print(f"Fit heuristic: {fit_heuristic}")
+            return True
+        return False
+
+    def get_fit_heuristic(self):
+        fit_ratio = self.fitting_parameters[:2]/np.array(self.initial_fitting_parameters[:2])
+        fit_heuristic_multiplicative = sum(fit_ratio + 1/fit_ratio)
+        fit_heuristic_additive = abs(self.fitting_parameters[3] - self.initial_fitting_parameters[3])
+        fit_heuristic = fit_heuristic_multiplicative + fit_heuristic_additive
+        return fit_heuristic
 
     def fit_plot_manually(self):
         continue_looping = True
@@ -149,7 +160,6 @@ class Detuning():
 
     def fit_plot_manually_iteration(self):
         fitting_input_choice = self.get_fitting_input_choice()
-        fitting_choice_functions = self.get_fitting_choice_functions()
         continue_looping, fit_rejected = self.execute_manual_fit_choice(fitting_input_choice)
         return continue_looping, fit_rejected
 
@@ -161,7 +171,8 @@ class Detuning():
         return fitting_input_choice
 
     def execute_manual_fit_choice(self, fitting_input_choice):
-        if fitting_input_choice in fitting_choice_functions:
+        if fitting_input_choice in self.fitting_choice_functions:
+            a = self.fitting_choice_functions[fitting_input_choice]
             fitting_choice_function, *args = self.fitting_choice_functions[fitting_input_choice]
             continue_looping, fit_rejected = fitting_choice_function(*args)
         else:
@@ -171,10 +182,10 @@ class Detuning():
 
     def update_fitting_parameter(self, parameter_index):
         parameter_index = int(parameter_index)
-        prompt = f"\nWhat is the new value of {self.parameter_names[parameter_index]}: "
+        prompt = f"\nWhat is the new value of '{self.parameter_names[parameter_index]}': "
         try:
             new_parameter_value = float(input(prompt))
-            fitting_parameters[parameter_index] = new_parameter_value
+            self.fitting_parameters[parameter_index] = new_parameter_value
         except:
             print("Sorry, that didn't work")
         return True, None
@@ -187,19 +198,45 @@ class Detuning():
 
     def update_all_fitting_parameters(self):
         prompt = "Enter all the new values in a list separated by spaces\n"
-        input_wrong = True
-        while input_wrong:
-            try:
-                fitting_parameters = [float(parameter_input) for parameter_input in input(prompt).split(" ")]
-                input_wrong = False
-            except:
-                print("Sorry, you typed it in wrong, try again")
+        self.set_all_fitting_parameters(prompt)
         continue_looping = True
         fit_rejected = None
         return continue_looping, fit_rejected
 
+    def set_all_fitting_parameters(self, prompt):
+        input_rejected = True
+        while input_rejected:
+            self.fitting_parameters, input_rejected = self.get_all_fitting_parameters(prompt)
+            input_rejected = self.check_if_4_fitting_parameters(input_rejected)
+
+    def get_all_fitting_parameters(self, prompt):
+        try:
+            fitting_parameters, input_wrong = self.attempt_get_fitting_parameters(prompt)
+        except:
+            fitting_parameters, input_wrong = self.failed_attempt_fitting_parameters()
+        return fitting_parameters, input_wrong
+
+    def attempt_get_fitting_parameters(self, prompt):
+        fitting_parameters = [float(parameter_input)
+                              for parameter_input in input(prompt).split(" ")]
+        input_wrong = False
+        return fitting_parameters, input_wrong
+
+    def failed_attempt_fitting_parameters(self):        
+        print("Sorry, you typed it in wrong, try again")
+        fitting_parameters = self.fitting_parameters
+        input_wrong = True
+        return fitting_parameters, input_wrong
+
+    def check_if_4_fitting_parameters(self, input_rejected):
+        if self.fitting_parameters is not None:
+            if len(self.fitting_parameters) != 4:
+                input_rejected = True
+                print("Sorry, you must enter 4 parameters")
+        return input_rejected
+
     def reattempt_automatic_fit(self):
-        self.get_automatic_fit(self.fitting_parameters)
+        self.fitting_parameters = self.get_automatic_fit(self.fitting_parameters)
         continue_looping = True
         fit_rejected = None
         return continue_looping, fit_rejected
@@ -219,17 +256,17 @@ class Detuning():
         for parameter_name, parameter_value in parameter_tuples:
             print(f"{parameter_name}: {parameter_value}")
 
-    def plot_S21(self, fitting=None):
+    def plot_S21(self, fitting=False):
         plt.figure()
         plt.plot(self.frequency_offset, self.S21, '.', alpha = 1)
         self.plot_fitting(fitting)
-        add_plot_labels()
+        self.add_plot_labels()
         plt.show()
 
     def plot_fitting(self, fitting):
-        if fitting is not None:
+        if fitting is True and self.fitting_parameters is not None:
             plt.plot(self.frequency_offset,
-                     self.evaluation_lorentzian(fitting),
+                     self.evaluate_lorentzian(self.fitting_parameters),
                      'k--', alpha=0.5, linewidth=2.0, label='fit')
 
     def add_plot_labels(self):
