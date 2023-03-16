795a97b0949940715f6d337e1d2d66ad652011fb
Henry Ginn <henryginn137@gmail.com>
2023-03-16 16:50:43 +0000

    Computed optimal subplot arrangement

--- Transmission Analysis/Plot.py
+++ Transmission Analysis/Plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 class Plot():
 
-    aspect_ratio = 16/9
+    aspect_ratio = 2/3
 
     def __init__(self, line_objects):
         self.line_objects = line_objects
@@ -21,21 +21,25 @@ class Plot():
         self.set_column_sizes()
 
     def set_row_sizes(self):
-        row_size = math.sqrt(self.count / self.aspect_ratio)
+        row_size = math.sqrt(self.count * self.aspect_ratio)
         self.row_small = math.floor(row_size)
         self.row_large = math.ceil(row_size)
 
     def set_column_sizes(self):
-        column_size = math.sqrt(self.count * self.aspect_ratio)
+        column_size = math.sqrt(self.count / self.aspect_ratio)
         self.column_small = math.floor(column_size)
         self.column_large = math.ceil(column_size)
 
     def set_row_column_pairs(self):
-        self.row_column_pairs = {(self.row_small, self.column_small): {},
-                                 (self.row_small, self.column_large): {},
-                                 (self.row_large, self.column_small): {},
-                                 (self.row_large, self.column_large): {}}
+        self.set_pairs()
+        self.row_column_pairs = {pair: {} for pair in self.pairs}
         self.populate_row_column_pairs()
+
+    def set_pairs(self):
+        self.pairs = [(self.row_small, self.column_small),
+                      (self.row_small, self.column_large),
+                      (self.row_large, self.column_small),
+                      (self.row_large, self.column_large)]
 
     def populate_row_column_pairs(self):
         for pair in self.row_column_pairs.keys():
@@ -43,22 +47,62 @@ class Plot():
 
     def get_row_column_pair_data(self, pair):
         size = pair[0] * pair[1]
-        aspect_ratio = pair[1] / pair[0]
+        aspect_ratio = pair[0] / pair[1]
         pair_data_dict = {"Size": size, "Aspect Ratio": aspect_ratio}
         return pair_data_dict
 
     def select_row_column_pair(self):
-        big_enough_pairs = {pair: pair_data for pair, pair_data in self.row_column_pairs.items()
-                            if pair_data["Size"] >= self.count}
-        aspect_ratio_dict = {pair_data["Aspect Ratio"]: pair
-                             for pair, pair_data in big_enough_pairs.items()}
-        best_pair = max(aspect_ratio_dict.items(), key=self.get_aspect_ratio_score)
-        self.rows, self.columns = best_pair[1]
+        if len(self.row_column_pairs) == 1:
+            self.exact_ratio_pair()
+        else:
+            self.non_exact_ratio_pairs()
 
-    def get_aspect_ratio_score(self, aspect_ratio_key_value):
-        aspect_ratio = aspect_ratio_key_value[0]
-        score = max(aspect_ratio/self.aspect_ratio, self.aspect_ratio/aspect_ratio)
+    def exact_ratio_pair(self):
+        self.rows, self.columns = self.pairs[0]
+
+    def non_exact_ratio_pairs(self):
+        is_big_enough_grid = self.get_is_big_enough_grid()
+        row_column_pair_functions = self.get_row_column_pair_functions()
+        row_column_pair_function, *args = row_column_pair_functions[is_big_enough]
+        row_column_pair_function(*args)
+        self.rows, self.columns = self.best_pair
+
+    def get_is_big_enough_grid(self):
+        is_big_enough_grid = tuple([row_column_pair["Size"] >= self.count
+                                    for row_column_pair in self.row_column_pairs.values()])
+        return is_big_enough_grid
+
+    def get_row_column_pair_functions(self):
+        row_column_pair_functions = {(True, True, True, True): (self.set_best_pair, 0),
+                                     (False, True, True, True): (self.set_best_pair, 1),
+                                     (False, True, False, True): (self.set_best_pair, 2),
+                                     (False, False, True, True): (self.middle_pair_compare,),
+                                     (False, False, False, True): (self.set_best_pair, 3)}
+        return row_column_pair_functions
+
+    def set_best_pair(self, pair_index):
+        self.best_pair = self.pairs[pair_index]
+
+    def middle_pair_compare(self):
+        self.set_aspect_ratio_scores()
+        self.compare_aspect_ratio_scores()
+
+    def set_aspect_ratio_scores(self):
+        aspect_ratio_1 = self.row_column_pairs[self.pairs[1]]["Aspect Ratio"]
+        aspect_ratio_2 = self.row_column_pairs[self.pairs[2]]["Aspect Ratio"]
+        self.aspect_ratio_score_1 = self.get_aspect_ratio_score(aspect_ratio_1)
+        self.aspect_ratio_score_2 = self.get_aspect_ratio_score(aspect_ratio_2)
+
+    def get_aspect_ratio_score(self, aspect_ratio):
+        score = max(aspect_ratio / self.aspect_ratio,
+                    self.aspect_ratio / aspect_ratio)
         return score
+
+    def compare_aspect_ratio_scores(self):
+        if self.aspect_ratio_score_1 < self.aspect_ratio_score_2:
+            self.set_best_pair(1)
+        else:
+            self.set_best_pair(2)
 
     def create_figure(self):
         fig, axes = plt.subplots(nrows=self.rows, ncols=self.columns)
--- Transmission Analysis/Transmission Analysis Interface.py
+++ Transmission Analysis/Transmission Analysis Interface.py
@@ -2,5 +2,5 @@ from DataSet import DataSet
 
 my_data_set = DataSet("14112022", 2)
 my_data_set.read_raw_transmission()
-my_data_set.plot_transmission("Raw", 2)
+my_data_set.plot_transmission("Raw", 11)
 my_data_set.align_transmission()
